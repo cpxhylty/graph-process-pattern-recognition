b8a24d5db7f592ba3b985f674b0efb60cac1daf6
胥浩宇 <18373542@buaa.edu.cn>
2021-03-12 21:36:46 +0800

    complete

--- src/ImageBasicProcess.java
+++ src/ImageBasicProcess.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -43,13 +44,11 @@ public class ImageBasicProcess {
         for (int i = 0; i < 256; i++) {
             numberOfEachIntensity[i] = 0;
         }
-        int wNow;
-        int hNow;
         Color colorNow;
         int intensityNow;
 //        count for each pixel
-        for (wNow = 0; wNow < width; wNow++) {
-            for (hNow = 0; hNow < height; hNow++) {
+        for (int wNow = 0; wNow < width; wNow++) {
+            for (int hNow = 0; hNow < height; hNow++) {
                 colorNow = new Color(image.getRGB(wNow, hNow));
                 intensityNow = colorNow.getRed();
                 numberOfEachIntensity[intensityNow] = numberOfEachIntensity[intensityNow] + 1;
@@ -109,7 +108,7 @@ public class ImageBasicProcess {
         return  imageProcessed;
     }
 
-    public static BufferedImage linearStretch(BufferedImage image) {
+    public static BufferedImage linearStretch(BufferedImage image) throws IOException {
         int width = image.getWidth();
         int height = image.getHeight();
         BufferedImage imageProcessed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -118,15 +117,29 @@ public class ImageBasicProcess {
         Color colorNow;
         int intensityNow;
         int newIntensity;
+        int[] numberOfEachIntensity = new int[256];
+        for (int i = 0; i < 256; i++) {
+            numberOfEachIntensity[i] = 0;
+        }
+//        linear stretch and count
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 colorNow = new Color(image.getRGB(i, j));
                 intensityNow = colorNow.getRed();
                 newIntensity = linearFunction(intensityNow);
+                numberOfEachIntensity[newIntensity]++;
                 onePixel[0] = newIntensity | newIntensity << 8 | newIntensity << 16;
                 raster.setDataElements(i, j, 1, 1, onePixel);
             }
         }
+//        draw new bar chart
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (Integer i = 0; i < 256; i++) {
+            dataset.addValue(numberOfEachIntensity[i], "1", i);
+        }
+        JFreeChart jFreeChart = ChartFactory.createBarChart("stretched bar chart", "intensity", "n", dataset);
+        FileOutputStream out = new FileOutputStream("/Users/xuhaoyu/大三下/图像处理与模式识别/stretched chart.png");
+        ChartUtils.writeChartAsPNG(out, jFreeChart, 900, 600);
         return imageProcessed;
     }
 
